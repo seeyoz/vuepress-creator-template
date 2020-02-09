f847b773486772949dc1661847c64da5fbba1d72
左撇峰子 <zpfz@seeyoz.cn>
2020-02-09 16:52:40 +0800

    no message
    Signed-off-by: 左撇峰子 <zpfz@seeyoz.cn>

--- docs/Menu_1/SubMenu_2/sidebar_contents.js
+++ docs/Menu_1/SubMenu_2/sidebar_contents.js
@@ -3,8 +3,8 @@ module.exports = [
 		title: "Custom Sidebar",
 		collapsable: true,
 		children:[
-			"/Menu_1/SubMenu_2/example_docs_1",
-			"/Menu_1/SubMenu_2/example_docs_2"
+			"example_docs_1",
+			"example_docs_2"
 		]
 	}
 ];
